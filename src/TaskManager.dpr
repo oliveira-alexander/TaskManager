22080b1d31955b16dc0bf6864f6e5085131620f4
Alexander Oliveira <marzi@Bjorn.local>
2025-03-24 18:18:09 -0300

    Criação do repositório e projeto inicial

---
+++ src/TaskManager.dpr
@@ -0,0 +1,14 @@
+program TaskManager;
+
+uses
+  Vcl.Forms,
+  View.Main in 'Views\View.Main.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
